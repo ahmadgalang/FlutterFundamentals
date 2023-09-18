ff0311d3970c3511888dfe25b7197b4d2f539a30
MacbookPro <macbookpro@MacbookPros-MacBook-Pro.local>
2023-09-18 07:48:35 +0700

    container

--- lib/container.dart
+++ lib/container.dart
@@ -19,11 +19,11 @@ class MyContainer extends StatelessWidget {
         children: [
           Center(
               child: Container(
-            padding: EdgeInsets.all(10),
+            padding: const EdgeInsets.all(10),
             width: 100,
             height: 100,
             decoration: BoxDecoration(
-              color: Color.fromARGB(255, 17, 87, 248),
+              color: const Color.fromARGB(255, 17, 87, 248),
               borderRadius: BorderRadius.circular(50.0),
               border:Border.all(width: 2)
             ),
@@ -31,7 +31,7 @@ class MyContainer extends StatelessWidget {
                 width: 90,
                 height: 90,
                 decoration: BoxDecoration(
-                    color: Color.fromARGB(255, 232, 229, 229),
+                    color: const Color.fromARGB(255, 232, 229, 229),
                     borderRadius: BorderRadius.circular(50.0),
                     border:Border.all(width: 2,color: Colors.blueGrey))),
           )),
