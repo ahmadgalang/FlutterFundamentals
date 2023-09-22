cc0fca24c75c0bbfe3498dda135ef1ae666f1f09
MacbookPro <macbookpro@MacbookPros-MacBook-Pro.local>
2023-09-22 10:41:06 +0700

    add file image

---
+++ lib/imagewidget.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyImageWidget());
+}
+
+class MyImageWidget extends StatelessWidget {
+  const MyImageWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Container(
+              width: 900,
+              height: 300,
+              color: Colors.black26,
+              child: Image(
+                image: AssetImage(
+                  'assets/images/ironman.jpg',
+                ),
+                fit: BoxFit.cover,
+                repeat: ImageRepeat.repeat,
+              )),
+        ),
+      ),
+    );
+  }
+}
